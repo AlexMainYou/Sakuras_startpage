d8b81e8b1473c2a22fa1eb446cd51e04d6d228cb
AlexMain <AlexMainYou@yandex.ru>
2025-06-24 14:20:25 +0400

    new tab-fix 3

--- webserver.py
+++ webserver.py
@@ -1,25 +1,21 @@
 import http.server
 import socketserver
 import os
+import sys
 
-# --- Новые строки для определения пути ---
-# Получаем путь к папке, в которой лежит этот скрипт
+# Устанавливаем текущую директорию
 script_path = os.path.dirname(os.path.abspath(__file__))
-# Меняем текущую рабочую директорию на папку со скриптом
 os.chdir(script_path)
-# ----------------------------------------
 
 HOST = "127.0.0.1"
-PORT = 9990 # Или любой другой свободный порт
+PORT = 1111
 
 Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer((HOST, PORT), Handler) as httpd:
-    print(f"Сервер запущен в папке: {script_path}")
-    print(f"Откройте в браузере: http://{HOST}:{PORT}")
-    print("Для остановки нажмите Ctrl+C")
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        print("\nСервер остановлен.")
-        httpd.shutdown()
+# Подавление любых выводов (чтобы не вызывать ошибки в pythonw)
+sys.stdout = open(os.devnull, 'w')
+sys.stderr = open(os.devnull, 'w')
+
+# Запуск сервера без обработки прерываний
+httpd = socketserver.TCPServer((HOST, PORT), Handler)
+httpd.serve_forever()
